4f60da306342a8509a2d97438ffe7c20e14e1c14
Jacob Thornton <31422741+jactho2025@users.noreply.github.com>
2023-10-05 21:14:16 +1300

    UPDATE #1: Began work on Startup UI

---
+++ Editor/GameProject/ProjectBrowserDialog.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Editor.GameProject
+{
+    /// <summary>
+    /// Interaction logic for ProjectBrowserDialog.xaml
+    /// </summary>
+    public partial class ProjectBrowserDialog : Window
+    {
+        public ProjectBrowserDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void OnToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == OpenProjectButton)
+            {
+                if(CreateProjectButton.IsChecked == true)
+                {
+                    CreateProjectButton.IsChecked = false;
+                    BrowserContent.Margin = new Thickness(0);
+                }
+                OpenProjectButton.IsChecked = true;
+            }
+            else
+            {
+                if (OpenProjectButton.IsChecked == true)
+                {
+                    OpenProjectButton.IsChecked = false;
+                    BrowserContent.Margin = new Thickness(-800,0,0,0);
+                }
+                CreateProjectButton.IsChecked = true;
+            }
+        }
+    }
+}
